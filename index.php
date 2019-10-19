a6156d1a53cb93df2529b3e40ba311d795d85919
janisprefect <hallo@janisvonseggern.de>
2019-10-19 16:45:16 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Prefect Kickstart</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="dist/styles/styles.css">
+    </head>
+<body>
+
+  <div class="uk-flex uk-flex-center uk-flex-middle uk-flex-column" uk-height-viewport>
+    <h1 class="uk-margin-remove prefect-color-primary">Prefect Kickstart</h1>
+
+    <p class="">web development environment kickstart project</p>
+    <p class="uk-text-small uk-text-muted uk-margin-remove">by <a href="https://twitter.com/janisprefect" class="prefect-color-inherit" target="_blank">janisprefect</a></p>
+  </div>
+
+  <script src="dist/scripts/scripts.js"></script>
+</body>
+</html>
